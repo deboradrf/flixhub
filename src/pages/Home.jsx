9089ae59b183dbc9ac80df46314d485e35d3416e
debora <fonsecadrf@outlook.com>
2025-08-21 13:52:49 -0300

    projeto finalizado

---
+++ src/pages/Home.jsx
@@ -0,0 +1,69 @@
+import MovieCard from "../components/MovieCard"
+import { useState, useEffect, use } from "react"
+import { searchMovies, getPopularMovies } from "../services/api"
+import "../css/Home.css"
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("")
+    const [movies, setMovies] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (error) {
+                setError("Falha ao carregadar os filmes...")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+
+        loadPopularMovies()
+    }, [])
+
+    const handleSearch = async (e) => {
+        e.preventDefault()
+
+        if (!searchQuery.trim()) return
+        if (loading) return
+
+        setLoading(true)
+        try {
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        } catch (error) {
+            setError("Falha ao carregadar os filmes...")
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Search for a movie" className="search-input" value={searchQuery} onChange= {(e) => setSearchQuery(e.target.value)}/>
+                <button type="submit" className="search-button">Buscar</button>
+            </form>
+
+            {error && <div className="error-message">{error}</div>}
+
+            {loading ? (
+                <div className="loading">Carregando...</div>
+            ) : (
+                <div className="movies-grid">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} key={movie.id}/>
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Home
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "f09d18403acef3e2d57efbb5910ea993"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json()
+    return data.results
+}
